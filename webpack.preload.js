b2cadd3302ce5723c815b0fec8ba21b854d1b273
Thuy Dao <thanhthuy.dao@uq.edu.au>
2026-04-24 13:16:35 +1000

    fix: remove unused preload file for python environment selection

--- webpack.preload.js
+++ webpack.preload.js
@@ -14,7 +14,6 @@ const preloadFiles = [
   'labview/preload.js',
   'settingsdialog/preload.js',
   'progressview/preload.js',
-  'pythonenvselectpopup/preload.js',
   'remoteserverselectdialog/preload.js',
   'titlebarview/preload.js',
   'updatedialog/preload.js',
