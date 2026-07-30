bba1900ccc75bbc5355f654777d3abf4148dd0fe
iishiishii <love_sweet_98@yahoo.com.vn>
2026-07-30 14:55:05 +1000

    rm auto update for tinyrange (archived)

--- scripts/neurodeskutil.js
+++ scripts/neurodeskutil.js
@@ -4,7 +4,6 @@ const path = require('path');
 const https = require('https');
 
 // const neurodesktomlFilePath = path.resolve(__dirname, '../neurodesktop.toml');
-const pkgjsonFilePath = path.resolve(__dirname, '../package.json');
 
 const cli = meow(
   `
@@ -13,20 +12,15 @@ const cli = meow(
 
     Options
       --set-neurodesk-version   set Neurodesk version
-      --set-tinyrange-version   set TinyRange version
     Other options:
       --help                     show usage information
 
     Examples
-      $ node neurodeskutil --set-neurodesk-version 
+      $ node neurodeskutil --set-neurodesk-version
 `,
   {
     flags: {
       setNeurodeskVersion: {
-        type: 'string',
-        default: ''
-      },
-      setTinyrangeVersion: {
         type: 'string',
         default: ''
       }
@@ -60,21 +54,4 @@ if (cli.flags.setNeurodeskVersion !== '') {
       console.error(error.message);
       process.exit(1);
     });
-}
-
-if (cli.flags.setTinyrangeVersion !== '') {
-  // parse application version
-  const pkgjsonFileData = fs.existsSync(pkgjsonFilePath)
-    ? fs.readJSONSync(pkgjsonFilePath)
-    : undefined;
-  if (!pkgjsonFileData) {
-    console.error('package.json not found!');
-    process.exit(1);
-  }
-
-  pkgjsonFileData['tinyrange_version'] = cli.flags.setTinyrangeVersion;
-  fs.writeFileSync(pkgjsonFilePath, JSON.stringify(pkgjsonFileData, null, 2));
-
-  console.log(`tinyrange version set to: ${cli.flags.setTinyrangeVersion}`);
-  process.exit(0);
 }
